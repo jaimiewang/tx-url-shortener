e3fabd396518087ecb049d638c190c6321d65e33
Lukasz Derlatka <toranktto@gmail.com>
2020-06-11 02:38:48 +0200

    Refactor main.go, remove old unnecessary GoLand files

--- src/main.go
+++ src/main.go
@@ -3,110 +3,92 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	"log"
 	"math/rand"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
-	"tx-url-shortener/api"
 	"tx-url-shortener/config"
 	"tx-url-shortener/database"
 	"tx-url-shortener/model"
-	"tx-url-shortener/view"
 )
 
-func initRouter() *mux.Router {
-	router := mux.NewRouter()
-	router.StrictSlash(true)
-	router.Use(handlers.ProxyHeaders)
-	router.Use(handlers.RecoveryHandler())
+func Setup(configFilename string) error {
+	rand.Seed(time.Now().UTC().UnixNano())
 
-	viewRouter := router.PathPrefix("").Subrouter()
-	apiRouter := router.PathPrefix("/api").Subrouter()
+	err := config.LoadConfig(configFilename)
+	if err != nil {
+		return err
+	}
 
-	apiRouter.Use(api.AuthHandler)
-	apiRouter.NotFoundHandler = api.NotFoundHandler()
+	err = database.InitDatabase()
+	if err != nil {
+		return err
+	}
 
-	viewRouter.HandleFunc("/{code}", view.ShortURLRedirectView)
-	apiRouter.HandleFunc("/urls", api.NewShortURLEndpoint).Methods("PUT")
-	apiRouter.HandleFunc("/urls/{code}", api.ShortURLEndpoint).Methods("GET")
+	err = model.InitModels()
+	if err != nil {
+		return err
+	}
 
-	return router
+	return nil
 }
 
-func generateAPIKey() {
+func GenerateAPIKey() (string, error) {
 	apiKey := &model.APIKey{
 		CreatedAt: time.Now().Unix(),
 	}
 
 	trans, err := database.DbMap.Begin()
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	err = apiKey.GenerateToken(trans)
 	if err != nil {
 		_ = trans.Rollback()
-		panic(err)
+		return "", err
 	}
 
 	err = trans.Insert(apiKey)
 	if err != nil {
 		_ = trans.Rollback()
-		panic(err)
+		return "", err
 	}
 
 	err = trans.Commit()
 	if err != nil {
 		_ = trans.Rollback()
-		panic(err)
+		return "", err
 	}
 
-	fmt.Printf("Your new API key is: %s\n", apiKey.Token)
+	return apiKey.Token, nil
 }
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	configFileFlag := flag.String("config", "config.yml", "Configuration file.")
-	generateAPIKeyFlag := flag.Bool("generate-api-key", false, "")
+	configFilename := flag.String("config", "config.yml", "Configuration file.")
+	generateAPIKey := flag.Bool("generate-api-key", false, "")
 	flag.Parse()
 
-	err := config.LoadConfig(*configFileFlag)
+	err := Setup(*configFilename)
 	if err != nil {
 		panic(err)
 	}
 
-	err = database.InitDatabase()
-	if err != nil {
-		panic(err)
-	}
+	if *generateAPIKey {
+		apiKey, err := GenerateAPIKey()
+		if err != nil {
+			panic(err)
+		}
 
-	err = model.InitModels()
-	if err != nil {
-		panic(err)
-	}
-
-	if *generateAPIKeyFlag {
-		generateAPIKey()
+		fmt.Printf("Your new API key is: %s\n", apiKey)
 		return
 	}
 
-	router := initRouter()
+	router := InitRouter()
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
 
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		log.Fatal(server.ListenAndServe())
-	}()
-	<-sc
-	_ = server.Shutdown(nil)
+	log.Fatal(server.ListenAndServe())
 }
